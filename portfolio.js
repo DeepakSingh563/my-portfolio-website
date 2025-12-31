fa322c9fc0a3575c5fafc29de424460b91376e52
deepak <kumarankit1731999@gmail.com>
2025-12-31 19:32:03 +0530

    update

--- portfolio.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector(".menu");
-const toggleBtn = document.querySelector(".menu-toggle");
-
-toggleBtn.addEventListener("click", () => {
-    menu.classList.toggle("active");
-});
